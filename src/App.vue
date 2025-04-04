9f118edc66be979246d99d965f5d5e9dc8fd8edd
nurik-nuxt <nurtuganulyn@gmail.com>
2025-04-04 15:49:25 +0500

    fix web worker

--- src/App.vue
+++ src/App.vue
@@ -4,43 +4,41 @@ import { IonPage, IonContent, IonButton } from '@ionic/vue'
 import Swiper from "./components/Swiper.vue";
 import HeaderBar from "./components/HeaderBar.vue";
 
-// Интерфейс для события beforeinstallprompt
 interface BeforeInstallPromptEvent extends Event {
   prompt: () => Promise<void>;
   userChoice: Promise<{ outcome: string; platform: string }>;
 }
 
-// Реф для сохранения события
 const deferredPrompt = ref<BeforeInstallPromptEvent | null>(null);
 
-// При монтировании компонента подписываемся на событие beforeinstallprompt
 onMounted(() => {
   window.addEventListener('beforeinstallprompt', (e: Event) => {
-    console.log('beforeinstallprompt')
     e.preventDefault();
+    console.log('Событие beforeinstallprompt сработало:', e);
     deferredPrompt.value = e as BeforeInstallPromptEvent;
   });
 });
 
-// Функция для показа диалога установки
 const installApp = async () => {
   if (deferredPrompt.value) {
     await deferredPrompt.value.prompt();
     const { outcome } = await deferredPrompt.value.userChoice;
     console.log(`Пользователь выбрал: ${outcome}`);
     deferredPrompt.value = null;
+  } else {
+    // Если событие не перехвачено, можно показать сообщение или инструкции
+    alert('Установка недоступна. Чтобы установить приложение, воспользуйтесь меню браузера.');
   }
 };
 </script>
 
 <template>
   <ion-page>
-    {{ deferredPrompt }} aaa
     <HeaderBar />
     <ion-content>
-      <!-- Кнопка установки отображается, если событие beforeinstallprompt перехвачено -->
+      <!-- Кнопка установки всегда видна -->
       <div style="padding: 16px;">
-        <ion-button v-if="deferredPrompt" @click="installApp">
+        <ion-button @click="installApp">
           Установить приложение
         </ion-button>
       </div>
